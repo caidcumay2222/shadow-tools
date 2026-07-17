5fba252e173ff7afe742e25d490fb0b5c58cfda1
caidcumay2222 <annguyencong25@gmail.com>
2026-07-17 10:47:50 +0700

    Update deleteHeader.js

--- js/deleteHeader.js
+++ js/deleteHeader.js
@@ -1,18 +1,12 @@
-// ========== Xóa Header xác thực RevenueCat ========== //
+// ========== Xóa Header xác thực ========== //
 // @nguyencongan
 
-// Xóa các header xác thực
 delete $request.headers["Authorization"];
 delete $request.headers["authorization"];
 delete $request.headers["X-Platform"];
 delete $request.headers["x-platform"];
-delete $request.headers["X-RevenueCat-API-Key"];
-delete $request.headers["x-revenuecat-api-key"];
-
-// Giữ nguyên body (nếu có)
-var body = $request.body || "{}";
 
 $done({
   headers: $request.headers,
-  body: body
+  body: $request.body || "{}"
 });
