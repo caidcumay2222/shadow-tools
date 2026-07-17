c6945c31682af4c198bd696675a8952d45ec50c6
caidcumay2222 <annguyencong25@gmail.com>
2026-07-17 10:21:02 +0700

    Update deleteHeader.js

--- js/deleteHeader.js
+++ js/deleteHeader.js
@@ -1,1 +1,14 @@
-$done({});
+// ========= Xóa Header xác thực ========= //
+// @nguyencongan
+
+let obj = JSON.parse($request.body);
+
+// Xóa các header xác thực để bypass RevenueCat
+delete $request.headers["Authorization"];
+delete $request.headers["X-Platform"];
+delete $request.headers["x-platform"];
+
+$done({
+  headers: $request.headers,
+  body: JSON.stringify(obj)
+});
